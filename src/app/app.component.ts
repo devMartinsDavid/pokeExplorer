b0df0fd7213bc7c26a33181fb6086776517382b7
devMartinsDavid <david_wallison@hotmail.com>
2025-06-18 18:44:18 -0300

    configuring ionic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {IonicModule} from "@ionic/angular"
 import { CommonModule } from '@angular/common';
+import {IonApp, IonRouterOutlet} from "@ionic/angular";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, IonicModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
