0d8185a03eb5657ce88bba73f4d55be8ac4feabc
devMartinsDavid <david_wallison@hotmail.com>
2025-06-18 16:43:38 -0300

    creating routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'details:id',
+    loadComponent: () => import('./pages/details/details.component').then(m => m.DetailsComponent)
+  },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./pages/favorites/favorites.component').then(m => m.FavoritesComponent)
+  }
 ];
