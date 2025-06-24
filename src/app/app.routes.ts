c716b92b81e124216809bb87641c9c5356344f8d
devMartinsDavid <david_wallison@hotmail.com>
2025-06-24 05:14:47 -0300

    refactor(routes): update routes to use AppLayout and remove dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,29 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent),
-    children: [
-      {
-        path: 'home',
-        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
-      },
-      {
-        path: 'favorites',
-        loadComponent: () => import('./pages/favorites/favorites.component').then(m => m.FavoritesComponent)
-      },
-      {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-      }
-    ]
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
-    path: 'hetails/:id',
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./pages/favorites/favorites.component').then(m => m.FavoritesComponent)
+  },
+  {
+    path: 'details/:id',
     loadComponent: () => import('./pages/details/details.component').then(m => m.DetailsComponent)
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'home'
   }
 ];
