b4faf5ee1ec19c8d2981f472b87380aa052c30e5
devMartinsDavid <david_wallison@hotmail.com>
2025-06-24 05:22:23 -0300

    import layout

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { AppLayoutComponent } from '../../shared/templates/app-layout/app-layout.component';
 
 @Component({
   selector: 'app-favorites',
   standalone:true,
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.scss'],
+  imports:[
+    AppLayoutComponent
+  ]
 })
 export class FavoritesComponent  implements OnInit {
 
