d0065346b2473d38a9003f5d55d9dbf5c8edab43
devMartinsDavid <david_wallison@hotmail.com>
2025-06-25 05:03:37 -0300

    feat: show global spinner on app init using LoadingService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 
 import { register } from 'swiper/element/bundle'
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { Observable } from 'rxjs';
+import { LoadingService } from './core/services/loading.service';
 register();
 
 @Component({
@@ -11,11 +14,20 @@ register();
   imports: [
     CommonModule,
     IonApp,
-    IonRouterOutlet
+    IonRouterOutlet,
+    SpinnerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'pokeExplorer';
+export class AppComponent implements OnInit, AfterViewInit {
+ title = 'pokeExplorer';
+ isLoading$:Observable<boolean>;
+
+  constructor(private loadingService: LoadingService) { this.isLoading$ = this.loadingService.loading$; }
+
+  ngOnInit() { this.loadingService.show(); }
+
+  ngAfterViewInit(): void { this.loadingService.hide(); }
+
 }
