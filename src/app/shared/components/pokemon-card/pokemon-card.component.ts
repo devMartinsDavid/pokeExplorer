63ea58279ff4f5acb7510eb2bbf602236391d820
devMartinsDavid <david_wallison@hotmail.com>
2025-06-26 15:06:38 -0300

    Fix selector and improve layout and event handling in PokemonCard component

--- src/app/shared/components/pokemon-card/pokemon-card.component.ts
+++ src/app/shared/components/pokemon-card/pokemon-card.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IonButton, IonCard, IonCardHeader, IonCardTitle, IonIcon } from '@ionic/angular/standalone';
+import { PokemonModel } from '../../../core/models/pokemon.model';
+
+import { addIcons } from 'ionicons';
+import { heart, heartOutline } from 'ionicons/icons';
+addIcons({
+  'heart': heart,
+  'heart-outline': heartOutline
+});
 
 @Component({
-  selector: 'app-pokemon-card',
+  selector: 'pokemon-card',
+  standalone: true,
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.scss'],
+  imports: [
+    CommonModule,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonButton,
+    IonIcon
+  ]
 })
-export class PokemonCardComponent  implements OnInit {
+export class PokemonCardComponent {
+  @Input({ required: true }) pokemon!: PokemonModel;
+  @Output() liked = new EventEmitter<PokemonModel>();
+  @Output() openDetails = new EventEmitter<number>();
 
   constructor() { }
 
-  ngOnInit() {}
+  toggleLike() { this.liked.emit(this.pokemon); }
+
+  showDetails() { this.openDetails.emit(this.pokemon.id); }
 
 }
